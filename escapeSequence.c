f157a56d67a357f1128aba1596bd74a45f48f674
Diego Lopez-Rodas <diegolopezrodas+github@gmail.com>
2025-06-10 15:19:49 -0400

    Add files via upload

---
+++ escapeSequence.c
@@ -0,0 +1,27 @@
+/**************************************
+
+    CREATED BY     : Diego Lopez-Rodas
+    CREATION DATE  : 06/10/2025
+    UPDATED DATE   : 06/10/2025
+    Description    : Following the "C Output - New lines" page's instructions from w3schools.
+
+ **************************************/
+
+#include <stdio.h>
+
+int main(void){
+
+    //Creating two new lines after printing statement
+    printf("Hello, World!\n\n");
+
+    //Creating a horizontal tab in the printing statement
+    printf("Hello, World!\tAnd the Internet!\n");
+
+    //Creating a blackslash in the prinitng statement
+    printf("Hello, World!\\And the Internet!\n");
+
+    //Printing double quotes in the printing statement
+    printf("Hello, \"World\"!");
+
+    return 0;
+}
---
+++ getStarted.c
@@ -0,0 +1,17 @@
+/**************************************
+
+    CREATED BY     : Diego Lopez-Rodas
+    CREATION DATE  : 06/09/2025
+    UPDATED DATE   : 06/09/2025
+    Description    : Following the "C Get Started" page's instructions from w3schools.
+
+ **************************************/
+
+#include <stdio.h>
+
+int main(void){
+
+    printf("Hello, World!");
+
+    return 0;
+}
