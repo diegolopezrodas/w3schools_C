109fc31b23e4e38ea0ef3661b70de0a81bc62941
Diego Lopez-Rodas <diegolopezrodas+github@gmail.com>
2025-07-11 17:19:57 -0400

    Add files via upload

---
+++ C Functions/mathFunctions.c
@@ -0,0 +1,22 @@
+/**************************************
+
+    CREATED BY     : Diego Lopez-Rodas
+    CREATION DATE  : 07/11/2025
+    UPDATED DATE   : 07/11/2025
+    Description    : Following the C Math Functions page's instructions from w3schools.
+
+ **************************************/
+
+#include <stdio.h>
+#include <math.h> //Must include math.h header file to use math functions
+
+int main(void){
+
+    printf("Calling sqrt():\n\n");
+    printf("The square root of 16: %.2f\n\n", sqrt(16));
+
+    printf("Calling pow():\n\n");
+    printf("4 to the power of 3: %.2f\n\n", pow(4, 3));
+
+    return 0;
+}
